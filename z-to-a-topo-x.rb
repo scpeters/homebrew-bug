6420b0a64bb5d2710d6481a8c552995657a59851
Steven Peters <scpeters@openrobotics.org>
2019-04-05 14:40:41 -0700

    z-to-a-topo-[xyz] bottles

--- z-to-a-topo-x.rb
+++ z-to-a-topo-x.rb
@@ -5,6 +5,12 @@ class ZToATopoX < Formula
   version "0.0"
   sha256 "c0ec2ebaa848201c77db2bf73294857555b4436cb1a31ee216b570cc69b535d4"
 
+  bottle do
+    root_url "https://osrf-distributions.s3.amazonaws.com/bottles-test"
+    cellar :any_skip_relocation
+    sha256 "bd23d6674600c49912895fb1c99e2859feab089331e4e91854c51f1f4c427fa0" => :high_sierra
+  end
+
   depends_on "z-to-a-topo-y"
 
   def install
--- z-to-a-topo-y.rb
+++ z-to-a-topo-y.rb
@@ -5,6 +5,12 @@ class ZToATopoY < Formula
   version "0.0"
   sha256 "c0ec2ebaa848201c77db2bf73294857555b4436cb1a31ee216b570cc69b535d4"
 
+  bottle do
+    root_url "https://osrf-distributions.s3.amazonaws.com/bottles-test"
+    cellar :any_skip_relocation
+    sha256 "ba6cb4ba3a9b67b05bba86d08e488921b35f067bc4067ef207deb12a9160d8e7" => :high_sierra
+  end
+
   depends_on "z-to-a-topo-z"
 
   def install
