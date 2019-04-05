f8e0edb1a905274e563a317fa9aa94965df961b3
Steven Peters <scpeters@openrobotics.org>
2019-04-05 11:52:15 -0700

    fix some audit issues

--- child_deprecated.rb
+++ child_deprecated.rb
@@ -1,8 +1,9 @@
 class ChildDeprecated < Formula
+  desc "Child formula depending on parent with deprecated option"
   homepage "https://github.com/scpeters/homebrew-bug"
   url "https://raw.githubusercontent.com/scpeters/homebrew-bug/master/README.md"
-  sha256 "c0ec2ebaa848201c77db2bf73294857555b4436cb1a31ee216b570cc69b535d4"
   version "0.0"
+  sha256 "c0ec2ebaa848201c77db2bf73294857555b4436cb1a31ee216b570cc69b535d4"
 
   depends_on "parent" => "option"
 
--- parent.rb
+++ parent.rb
@@ -1,11 +1,12 @@
 class Parent < Formula
+  desc "Parent formula illustrating deprecation bug"
   homepage "https://github.com/scpeters/homebrew-bug"
   url "https://raw.githubusercontent.com/scpeters/homebrew-bug/master/README.md"
-  sha256 "f7ce5ecd90947b9f5be6ce7d51d86ff69dbb3f6db2003e3065d2a235e1d11491"
   version "0.0"
+  sha256 "f7ce5ecd90947b9f5be6ce7d51d86ff69dbb3f6db2003e3065d2a235e1d11491"
 
-  deprecated_option "option" => "with-option"
   option "with-option"
+  deprecated_option "option" => "with-option"
 
   def install
     system "echo", "option" if build.with? "option"
